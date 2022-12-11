4537600410622e25353a32d713a2c820b3eac6b6
padi-dev-hoangnd2 <hoangnd2@paditech.com>
2022-12-12 00:15:33 +0700

    push

---
+++ core/Router.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\core;
+
+class Router{
+
+    public Request $request;
+    protected array $routes = [];
+
+    public function __construct($request){
+        $this->request = $request;
+    }
+
+    public function get($path, $callback){
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve(){
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if ($callback === false){
+            return "Not Found";
+            
+        }
+        
+        if (is_string($callback)){
+            return $this->renderView($callback);
+        }
+        
+        echo call_user_func($callback);
+
+      
+
+    }
+    public function renderView(){
+            
+    }
+}
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once __DIR__.'/vendor/autoload.php';
+
+    use app\core\Application ;
+
+    $app = new Application();
+
+    $app->router->get('/',function(){
+        return "hello";
+    });
+
+    $app->router->get('/contact','contact');
+
+    $app->run();
+
+?>
