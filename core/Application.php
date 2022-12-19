55e6240563b54982e1b34fb8876a546b7fdce35d
ndhoangg <nd.hoangg@gmail.com>
2022-12-19 11:13:26 +0700

    push

--- core/Application.php
+++ core/Application.php
@@ -1,38 +1,37 @@
 <?php
 
-namespace app\core;
+    namespace app\core;
 
+    class Application{
 
-class Application{
+        public static $ROOT_DIR;
+        public $router;
+        public $request;
+        public $response;
+        public static $app;
+        public $controller;
 
-    public static $ROOT_DIR;
-    public $router;
-    public $request;
-    public $response;
-    public static $app;
-    public $controller;
+        public function __construct($rootPath){
+            self::$ROOT_DIR = $rootPath;
+            self::$app = $this;
+            $this->request = new Request();
+            $this->response = new Response();
+            $this->router = new Router($this->request,$this->response);
+        }
 
-    public function __construct($rootPath){
-        self::$ROOT_DIR = $rootPath;
-        self::$app = $this;
-        $this->request = new Request();
-        $this->response = new Response();
-        $this->router = new Router($this->request,$this->response);
+        //Run App
+        public function run(){
+            $this->router->resolve();
+        }
+
+        public function getController(){
+            return $this->controller;
+        }
+
+        public function setController(Controller $controller){
+            $this->controller = $controller;
+        }
+        
     }
-
-    //Run App
-    public function run(){
-        $this->router->resolve();
-    }
-
-    public function getController(){
-        return $this->controller;
-    }
-
-    public function setController(Controller $controller){
-        $this->controller = $controller;
-    }
-
-}
 
 ?>
